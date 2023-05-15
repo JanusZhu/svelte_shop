0de9e359c82af0ecb182b78d89d1ea8605073e0c
JanusZhu <janus.zhu@yale.edu>
2023-05-14 23:22:38 -0400

    Updated Shop page

--- src/components/Shop.svelte
+++ src/components/Shop.svelte
@@ -2,20 +2,23 @@
   import Items from './products/Items.svelte'
   import Category from './products/Category.svelte'
   import products from '../data.json';
+  let selectedCategory = "all"
 
   let filteredProducts = products
   const handleFilter= (category)=>{
     if (category === "all"){
       filteredProducts = products;
+      selectedCategory = 'all'
     }
     else{
     filteredProducts = products.filter((product) => product.category.includes(category));
+    selectedCategory = category
     }
   }
 </script>
 
 <main>
-  <Category {handleFilter}/>
+  <Category {handleFilter} {selectedCategory}/>
   <Items {filteredProducts}/>
 
 </main>
--- src/components/products/Category.svelte
+++ src/components/products/Category.svelte
@@ -1,8 +1,11 @@
 <script>
     export let handleFilter;
+    export let selectedCategory
     let categories = ['all',"men's clothing", 'jewelry', 'electronics', "women's clothing"]
 </script>
 <main>
+    <div class="header"><h2>Shop /</h2><h1>{selectedCategory.toUpperCase()}</h1></div>
+
 
     {#each categories as category (category)}
          <a class="category" on:click={()=>{handleFilter(category)}}>{category.toUpperCase()}</a>
@@ -18,6 +21,18 @@
         padding-left: 6rem;
         display: flex;
         flex-direction: column;
+        align-items: flex-start;
+    }
+    h1, h2 {
+        padding: 0;
+        text-align: left;
+    }
+    .header{
+        height:150px;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        margin-bottom: 4rem;
     }
     .category {
         text-align: left;
